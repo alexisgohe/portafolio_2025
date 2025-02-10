8a96a654774eb5c2efadb13d5328739b92c1c213
Alexis <alexisgh74@gmail.com>
2025-02-09 20:41:18 -0600

    FIX - Se crea encabezado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ThemeService } from '../service/theme.service';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faGithub, faLinkedinIn, faCloudflare, faMailchimp } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  faGithub = faGithub;
+  faLinkedinIn = faLinkedinIn;
+  faCloudflare = faCloudflare;
+  faMailchimp = faMailchimp;
+
   isDarkMode = false;
 
   constructor(private themeService: ThemeService) {
--- src/app/app.config.server.ts
+++ src/app/app.config.server.ts
@@ -1,10 +1,12 @@
-import { mergeApplicationConfig, ApplicationConfig } from '@angular/core';
+import { mergeApplicationConfig, ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideServerRendering } from '@angular/platform-server';
 import { appConfig } from './app.config';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 const serverConfig: ApplicationConfig = {
   providers: [
     provideServerRendering(),
+    importProvidersFrom(FontAwesomeModule)
   ]
 };
 
