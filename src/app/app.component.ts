589ce8b011ddc389cf6758f1a2fe27e84228da54
Alexis <alexisgh74@gmail.com>
2026-03-19 10:41:33 -0600

    ADD - Nuevos proyectos a portafolio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -76,10 +76,14 @@ export class AppComponent {
   private updateTranslations() {
     this.translate
       .get([
+        'SAMA',
+        'SAMA_DESC',
+        'SAMA_INI',
+        'SAMA_FIN',
         'SEFIPLAN',
         'PROGRAMMER',
         'SEFIPLAN_DESC',
-        'CURRENT',
+        'SEFIPLAN_FIN',
         'JUNE',
         'OCTOBER',
         'HOSPITAL_ANGELES',
@@ -95,16 +99,36 @@ export class AppComponent {
         'PROJECT_VEHICULOS_DESC',
         'PROJECT_PRODUCTORES_TITLE',
         'PROJECT_PRODUCTORES_DESC',
+        'PROJECT_SMERA_TITLE',
+        'PROJECT_SMERA_DESC',
+        'PROJECT_PDF_TOOLS_TITLE',
+        'PROJECT_PDF_TOOLS_DESC',
+        'PROJECT_TOURIFY_TITLE',
+        'PROJECT_TOURIFY_DESC',
+        'PROJECT_LOCKLY_TITLE',
+        'PROJECT_LOCKLY_DESC',
+        'PROJECT_CONSULTORIA_TITLE',
+        'PROJECT_CONSULTORIA_DESC',
+        'PROJECT_POS_TITLE',
+        'PROJECT_POS_DESC',
       ])
       .subscribe((translations) => {
         this.experiencias = [
+          {
+            compania: translations['SAMA'],
+            titulo: translations['PROGRAMMER'],
+            descripcion: translations['SAMA_DESC'],
+            inicio: translations['SAMA_INI'],
+            fin: translations['SAMA_FIN'],
+            icono: 'code',
+          },
           {
             compania: translations['SEFIPLAN'],
             titulo: translations['PROGRAMMER'],
             descripcion: translations['SEFIPLAN_DESC'],
             inicio: translations['JUNE'],
-            fin: translations['CURRENT'],
-            icono: 'dns',
+            fin: translations['SEFIPLAN_FIN'],
+            icono: 'code',
           },
           {
             compania: translations['HOSPITAL_ANGELES'],
@@ -112,7 +136,7 @@ export class AppComponent {
             descripcion: translations['HOSPITAL_DESC'],
             inicio: translations['OCTOBER'],
             fin: translations['JUNE'],
-            icono: 'code',
+            icono: 'dns',
           },
         ];
         this.proyectos = [
@@ -172,6 +196,88 @@ export class AppComponent {
             url: 'assets/restaurante.jpg',
             button: true,
           },
+          {
+            title: translations['PROJECT_SMERA_TITLE'],
+            description: translations['PROJECT_SMERA_DESC'],
+            technologies: ['NextJS', 'Node.js', 'Appwrite'],
+            technologiesImg: [
+              'assets/icons/nextjs.svg',
+              'assets/icons/nodedotjs.svg',
+              'assets/icons/appwrite.svg',
+            ],
+            url: 'assets/smera.jpg',
+            button: false,
+          },
+          {
+            title: translations['PROJECT_PDF_TOOLS_TITLE'],
+            description: translations['PROJECT_PDF_TOOLS_DESC'],
+            technologies: ['NextJS', 'Node.js', 'python'],
+            technologiesImg: [
+              'assets/icons/nextjs.svg',
+              'assets/icons/nodedotjs.svg',
+              'assets/icons/python.svg',
+            ],
+            url: 'assets/pdf_tools.jpg',
+            button: false,
+          },
+          {
+            title: translations['PROJECT_TOURIFY_TITLE'],
+            description: translations['PROJECT_TOURIFY_DESC'],
+            technologies: ['NextJS', 'Node.js', 'Supabase'],
+            technologiesImg: [
+              'assets/icons/nextjs.svg',
+              'assets/icons/nodedotjs.svg',
+              'assets/icons/supabase.svg',
+            ],
+            url: 'assets/tourify.jpg',
+            button: false,
+          },
+          {
+            title: translations['PROJECT_LOCKLY_TITLE'],
+            description: translations['PROJECT_LOCKLY_DESC'],
+            technologies: ['NextJS', 'Node.js', 'Supabase'],
+            technologiesImg: [
+              'assets/icons/nextjs.svg',
+              'assets/icons/nodedotjs.svg',
+              'assets/icons/supabase.svg',
+            ],
+            url: 'assets/lockly.jpg',
+            button: false,
+          },
+          {
+            title: translations['PROJECT_CONSULTORIA_TITLE'],
+            description: translations['PROJECT_CONSULTORIA_DESC'],
+            technologies: ['NextJS', 'Node.js'],
+            technologiesImg: [
+              'assets/icons/nextjs.svg',
+              'assets/icons/nodedotjs.svg',
+            ],
+            url: 'assets/consultoria.jpg',
+            button: false,
+          },
+          {
+            title: translations['PROJECT_POS_TITLE'],
+            description: translations['PROJECT_POS_DESC'],
+            technologies: ['NextJS', 'Node.js', 'Spring'],
+            technologiesImg: [
+              'assets/icons/nextjs.svg',
+              'assets/icons/nodedotjs.svg',
+              'assets/icons/spring.svg',
+            ],
+            url: 'assets/pos.jpg',
+            button: false,
+          },
+          {
+            title: translations['PROJECT_CONSULTORIA_TITLE'],
+            description: translations['PROJECT_CONSULTORIA_DESC'],
+            technologies: ['NextJS', 'Node.js'],
+            technologiesImg: [
+              'assets/icons/nextjs.svg',
+              'assets/icons/nodedotjs.svg',
+            ],
+            url: 'assets/consultoria.jpg',
+            button: false,
+          }
         ];
       });
   }
